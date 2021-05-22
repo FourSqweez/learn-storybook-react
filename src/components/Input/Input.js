75a8b512fc9da11ccbc3fad1696a7d7c8a2a91bf
FourSqweez <four7518@gmail.com>
2021-05-22 17:06:41 +0700

    ADD : Create Input story and add to Form group

---
+++ src/components/Input/Input.js
@@ -0,0 +1,12 @@
+import './Input.css'
+
+const Input = (props) => {
+   const {size = 'medium', ...rest} = props
+   return (
+      <input className={`input ${size}`} {...rest}>
+         
+      </input>
+   )
+}
+
+export default Input
